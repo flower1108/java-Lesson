83a761b8ddb34c07add8c76c7201c9fbf4e93676
flower1108 <129576720+flower1108@users.noreply.github.com>
2023-07-25 02:21:45 +0000

    start

---
+++ lesson1.java
@@ -0,0 +1,10 @@
+public class Hero {
+  private string name;
+  private int hp, mp;
+
+  public String toString() {
+    return "勇者(名前=" + this.name
+      + "/HP=" + this.hp + "/MP=" + this.mp + ")";
+  }
+  :
+}
