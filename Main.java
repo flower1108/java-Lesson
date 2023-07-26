7b68cbb3e1f59f8d335852846d9e751c27d38ebb
flower1108 <129576720+flower1108@users.noreply.github.com>
2023-07-26 13:03:56 +0000

    java-lesson

--- Main.java
+++ Main.java
@@ -5,8 +5,8 @@ public class Main {
     System.out.println("韓国が好きです");
     System.out.println("31 + 31の計算をします");
     System.out.println(31 + 31);
-    System.out.println(52 - 21);
-    System.out.println(54 * -5);
-    System.out.println("こたえは" + 64);
+    int x;
+    x = 6;
+    System.out.println(x * x * 3.14);
   }
 }
