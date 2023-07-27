bd95dc3adb9149ebcf9427056a46221294f04b0f
flower1108 <129576720+flower1108@users.noreply.github.com>
2023-07-27 02:33:36 +0000

    java-test

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test {
+  public static void main(String[] arg) {
+    int a = 3;
+    int b = 5;
+    int c = a * b;
+    System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + c);
+  }
+}
