1b9fe271f34bad3a845364f3630ff09fa1980176
flower1108 <129576720+flower1108@users.noreply.github.com>
2023-07-28 07:08:52 +0000

    java-lesson

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main {
   public static void main(String[] args) {
-    float f = 3;
-    double d = f;
-    System.out.println(f);
+    double d = 8.5 / 2;
+    long l = 5 + 2L;
     System.out.println(d);
+    System.out.println(l);
   }
 }
