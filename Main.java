d201e5ec87d995252a432a1b66aa96c0b70bd9b0
flower1108 <129576720+flower1108@users.noreply.github.com>
2023-07-29 04:46:13 +0000

    java-lesson

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("あなたの名前を入力してください。");
-    String name = new java.util.Scanner(System.in).nextLine();
-    System.out.println("あなたの年齢を入力してください。");
-    int age = new java.util.Scanner(System.in).nextInt();
-    System.out.println("ようこそ、" + age + "歳の" + name + "さん");
+    boolean tenki = true;
+    if (tenki == true) {
+      System.out.println("洗濯をします");
+      System.out.println("散歩にいきます");
+    } else {
+      System.out.println("DVDを見ます");
+    }
   }
 }
