5a54c176e351486a8315ccf1160d9ced5767e96a
flower1108 <129576720+flower1108@users.noreply.github.com>
2023-08-03 07:10:11 +0000

    java-test

--- Test.java
+++ Test.java
@@ -1,14 +1,12 @@
 public class Test {
   public static void main(String[] arg) {
-    int isHungry = 1;
-    String food = "りんご";
-    System.out.println("こんにちは");
-    if (isHungry == 0) {
-      System.out.println("お腹がいっぱいです");
+    boolean tenki = true;
+    if (tenki == true) {
+      System.out.println("洗濯をします");
+      System.out.println("散歩をします");
     } else {
-      System.out.println("はらぺこです");
-      System.out.println(food + "をいただきます");
-      System.out.println("ごちそうさまでした");
+      System.out.println("DVDを見ます");
+      System.out.println("寝ます");
     }
   }
 }
