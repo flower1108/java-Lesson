0375a635431e208d763979abce61ead7ddc9d645
flower1108 <129576720+flower1108@users.noreply.github.com>
2023-08-05 12:58:44 +0000

    java-lesson

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
   public static void main(String[] args) {
-    int [] scores = new int[5];
-    System.out.println(scores[0]);
+    int [] scores;
+    scores = new int[5];
+    scores[1]= 30;
+    System.out.println(scores[1]);
   }
 }
