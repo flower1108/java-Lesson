c2a73baf365a7c76d3fd0701b58b22584ccc4952
flower1108 <129576720+flower1108@users.noreply.github.com>
2023-08-07 05:11:53 +0000

    java-lesson

--- Main.java
+++ Main.java
@@ -1,8 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-    int[] scores = {20, 30, 40, 50, 80};
-    for (int value : scores) {
-      System.out.println(value);
-    }
+    int[] array = {1, 2, 2};
+    array = null;
+    array[0] = 10;
   }
 }
