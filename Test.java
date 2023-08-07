4a7515b4a563083be42972dba77a3443c017a493
flower1108 <129576720+flower1108@users.noreply.github.com>
2023-08-07 05:28:39 +0000

    java-test

--- Test.java
+++ Test.java
@@ -1,8 +1,11 @@
 public class Test {
   public static void main(String[] arg) {
-    int[] points = new int[4];
-    double[] weights = new double[5];
-    boolean[] answers = new boolean[3];
-    String[] names = new String[3];
+    int[] moneyList = {121902, 8302, 55100};
+    for (int i = 0; i < moneyList.length; i++) {
+      System.out.println(moneyList[i]);
+    }
+    for (int money : moneyList) {
+      System.out.println(money);
+    }
   }
 }
