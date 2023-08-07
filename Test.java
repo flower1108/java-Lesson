6ef0c48aa900eb93f0fcbd4db4fddc008f3d617c
flower1108 <129576720+flower1108@users.noreply.github.com>
2023-08-07 05:32:15 +0000

    java-test

--- Test.java
+++ Test.java
@@ -1,11 +1,12 @@
 public class Test {
   public static void main(String[] arg) {
-    int[] moneyList = {121902, 8302, 55100};
-    for (int i = 0; i < moneyList.length; i++) {
-      System.out.println(moneyList[i]);
-    }
-    for (int money : moneyList) {
-      System.out.println(money);
+    int[] numbers = {3, 4, 9};
+    System.out.println("1桁の数字を入力してください");
+    int input = new java.util.Scanner(System.in).nextInt();
+    for (int n : numbers) {
+      if (n == input) {
+        System.out.println("アタリ！");
+      }
     }
   }
 }
