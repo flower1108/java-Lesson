118bcf3332b3dc77204d6d1f05405732b5aba21c
flower1108 <129576720+flower1108@users.noreply.github.com>
2023-08-09 12:27:54 +0000

    java-lesson

--- Main.java
+++ Main.java
@@ -1,10 +1,16 @@
 public class Main {
   public static int add(int x, int y) {
-    int ans = x + y;
-    return ans;
+    return x + y;
+  }
+  public static double add(double x, double y) {
+    return x + y;
+  }
+  public static String add(String x, String y) {
+    return x + y;
   }
   public static void main(String[] args) {
-    int ans = add (100, 10);
-    System.out.println(add(add(10, 20), add(30, 40)));
+    System.out.println(add(10, 20));
+    System.out.println(add(3.5, 2.7));
+    System.out.println(add("Hello", "World"));
   }
 }
