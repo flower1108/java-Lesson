f9a0de3d484dabb7ae49f643c3526cd25bef4bcd
flower1108 <129576720+flower1108@users.noreply.github.com>
2023-08-21 07:34:07 +0000

    java-test

--- Test.java
+++ Test.java
@@ -1,15 +1,17 @@
 public class Test {
-  public static void main(String[] arg) {
-    introduceOneseIf();
+  public static void main(String[] args) {
+    String address = "uso800@xxxx.com";
+    String text= "今度、飲みに行きませんか";
+    email(address, text);
   }
-  public static void introduceOneseIf() {
-    String name = "Minato";
-    int age = 22;
-    double height = 169.9;
-    char zodiac = '辰';
-    System.out.println("私の名前は" + name + "です");
-    System.out.println("歳は" + age + "です");
-    System.out.println("身長は" + height + "です");
-    System.out.println("干支は" + zodiac + "です");
+  public static void email(String address, String text) {
+    System.out.println(address + " に、以下のメールを送信しました");
+    System.out.println("件名：無題");
+    System.out.println("本文：" + text);
+  }
+  public static void email(String title, String address, String text) {
+    System.out.println(address + "に、以下のメールを送信しました");
+    System.out.println("件名：" + title);
+    System.out.println("本文：" + text);
   }
 }
