6c98f723f91aa2790f76604d70ee18be435941d9
flower1108 <129576720+flower1108@users.noreply.github.com>
2023-08-29 02:22:33 +0000

    java-lesson

--- Main.java
+++ Main.java
@@ -1,13 +1,9 @@
 public class Main {
   public static void main(String[] args) {
-    String title = "お誘い";
-    String address = "uso800@xxxx.com";
-    String text= "今度、飲みに行きませんか";
-    email(title, address, text);
-  }
-  public static void email(String title, String address, String text) {
-    System.out.println(address + " に、以下のメールを送信しました");
-    System.out.println("件名：" + title);
-    System.out.println("本文：" + text);
+    int[] heights = {172, 149, 152, 191, 155};
+    java.util.Arrays.sort(heights);
+    for (int h : heights) {
+      System.out.println(h);
+    }
   }
 }
